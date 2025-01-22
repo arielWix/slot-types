ea3d73079094f578026cfb3f5aefc2938e349fe3
Ariel Hadad <arielh@wix.com>
2025-01-22 13:15:17 +0200

    Update 20dc3114-3af7-4783-b9e1-ccc893b8f146-type.d.ts

--- 20dc3114-3af7-4783-b9e1-ccc893b8f146-type.d.ts
+++ 20dc3114-3af7-4783-b9e1-ccc893b8f146-type.d.ts
@@ -3,7 +3,7 @@ export interface CheckoutComponentProps {
      * The ID of the current checkout process.
      */
     checkoutId: string;
-  
+
     /**
      * The ID of the step currently rendered in the checkout page.
      * Can be one of the following:
@@ -13,20 +13,24 @@ export interface CheckoutComponentProps {
      * - 'place-order'
      */
     stepId: 'contact-details' | 'delivery-method' | 'payment-and-billing' | 'place-order';
-  }
+}
 
 export interface ProductsComponentProps {
     /**
-     * The ID of the product
+     * The ID of the product.
      */
     productId: string;
-  }
-
+}
 
 export interface OrderComponentProps {
-   /**
-     * The ID of the order
+    /**
+     * The ID of the order.
      */
     orderId: string;
-    
+
+    /**
+     * Callback that notifies the host page about an order update, prompting a UI refresh. 
+     * It returns a promise indicating when the UI update on the host page is complete.
+     */
+    onOrderUpdate: () => Promise<void>;
 }
